5081befbdb4777d9fc9fdb4eeadde7fbe2881dd2
semoro <sem-oro@yandex.ru>
2015-05-22 22:21:17 +0300

    Crash detection

--- core/src/net/xcodersteam/eengineer/MainGameScreen.java
+++ core/src/net/xcodersteam/eengineer/MainGameScreen.java
@@ -238,6 +238,7 @@ public class MainGameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
         Gdx.gl.glClearColor(235f / 255f, 218f / 255f, 159f / 255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         mDelt += delta;
@@ -252,11 +253,12 @@ public class MainGameScreen implements Screen {
         renderGui();
         renderer.end();
         stageBatch.begin();
-        renderSecondPass(x, y);
 
+        renderSecondPass(x, y);
         stageBatch.end();
         stage.act(delta);
         stage.draw();
+        throw new RuntimeException("D");
     }
 
 
